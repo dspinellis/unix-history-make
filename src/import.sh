86666327e8c8bbd0ff466140da5afa67c48228da
Diomidis Spinellis <dds@aueb.gr>
2014-05-20 18:32:37 +0300

    Run the commands in the correct directory

--- src/import.sh
+++ src/import.sh
@@ -18,18 +18,15 @@ git commit -a -m "Add license"
 # Release branch
 git branch Research-Release
 
-
-cd ..
 git branch Research-Development-v5
-SHA=`cd import; git rev-parse Research-Release`
-perl import-dir.pl -m $SHA -c v5.map $OLD_UNIX/v5 Research V5 -0500 |
-more
-exit
-(cd import ; git fast-import --stats --done --quiet)
+SHA=`git rev-parse Research-Release`
+(cd .. ; perl import-dir.pl -m $SHA -c v5.map $OLD_UNIX/v5 Research V5 -0500 ) |
+tee foo |
+git fast-import --stats --done --quiet
 
 git branch Research-Development-v6
-SHA=`cd import; git rev-parse Research-Release`
-perl import-dir.pl -m $SHA -c v6.map $OLD_UNIX/v6 Research V6 -0500 |
-(cd import ; git fast-import --stats --done --quiet)
+SHA=`git rev-parse Research-Release`
+(cd .. ; perl import-dir.pl -m $SHA -c v5.map $OLD_UNIX/v6 Research V6 -0500 ) |
+git fast-import --stats --done --quiet
 
 #git repack --window=50 -a -d -f
