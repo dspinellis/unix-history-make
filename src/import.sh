86febdb3208900558ba933cd3685af08130a7dfc
Diomidis Spinellis <dds@aueb.gr>
2014-05-20 18:37:21 +0300

    Convert sequence into a loop

--- src/import.sh
+++ src/import.sh
@@ -18,15 +18,13 @@ git commit -a -m "Add license"
 # Release branch
 git branch Research-Release
 
-git branch Research-Development-v5
-SHA=`git rev-parse Research-Release`
-(cd .. ; perl import-dir.pl -m $SHA -c v5.map $OLD_UNIX/v5 Research V5 -0500 ) |
-tee foo |
-git fast-import --stats --done --quiet
+for i in {5,6}
+do
+	git branch Research-Development-v$i
+	SHA=`git rev-parse Research-Release`
+	(cd .. ; perl import-dir.pl -m $SHA -c v$i.map $OLD_UNIX/v$i Research V$i -0500 ) |
+	git fast-import --stats --done --quiet
+done
 
-git branch Research-Development-v6
-SHA=`git rev-parse Research-Release`
-(cd .. ; perl import-dir.pl -m $SHA -c v5.map $OLD_UNIX/v6 Research V6 -0500 ) |
-git fast-import --stats --done --quiet
 
 #git repack --window=50 -a -d -f
