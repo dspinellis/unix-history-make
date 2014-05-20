510f9159caf814199f82e5d1a7842afa2be19f3c
Diomidis Spinellis <dds@aueb.gr>
2014-05-20 17:06:17 +0300

    Parameterize archive lcoation

--- src/import.sh
+++ src/import.sh
@@ -3,6 +3,9 @@
 # Import Unix branches into a single repo
 #
 
+# Location of archive mirror
+OLD_UNIX=$UH/vol/nbk/old-unix
+
 # Initialize repo
 rm -rf import
 mkdir import
@@ -19,11 +22,13 @@ git branch Research-Release
 
 cd ..
 SHA=`cd import; git rev-parse Research-Release`
-perl import-dir.pl -m $SHA -c v5.map /vol/nbk/old-unix/v5 Research V5 -0500 |
+perl import-dir.pl -m $SHA -c v5.map $OLD_UNIX/v5 Research V5 -0500 |
+more
+exit
 (cd import ; git fast-import --stats --done --quiet)
 
 SHA=`cd import; git rev-parse Research-Release`
-perl import-dir.pl -m $SHA -c v6.map /vol/nbk/old-unix/v6 Research V6 -0500 |
+perl import-dir.pl -m $SHA -c v6.map $OLD_UNIX/v6 Research V6 -0500 |
 (cd import ; git fast-import --stats --done --quiet)
 
 #git repack --window=50 -a -d -f
