939295b3674533759bcf66471ed6bd51dbd816ba
Diomidis Spinellis <dds@aueb.gr>
2014-05-20 22:04:38 +0300

    Verify release contents

--- src/import.sh
+++ src/import.sh
@@ -28,3 +28,30 @@ done
 
 
 #git repack --window=50 -a -d -f
+
+# Succeed if text files in the two specified directories
+# are the same
+same_text()
+{
+	diff -r "$1" "$2" |
+	perl -ne '
+		BEGIN {$exit = 0}
+		chop;
+		if (!s/^Only in // || !s|: |/| || -T) {
+			$exit = 1;
+			print "$_\n"
+		}
+		END {exit $exit}'
+}
+
+# Verify releases are the same
+for i in {5,6}
+do
+	git checkout Research-Development-v$i
+	if ! same_text . $OLD_UNIX/v$i
+	then
+		echo "Differences found" 1>&2
+		exit 1
+	fi
+done
+
