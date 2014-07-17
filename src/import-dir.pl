64d338ab2288b640dfc2c27fbd73622d6bf65066
Diomidis Spinellis <dds@aueb.gr>
2014-07-17 16:36:38 +0300

    Correct handling of import branch in git imports

--- src/import-dir.pl
+++ src/import-dir.pl
@@ -131,7 +131,8 @@ my $tz_offset = shift unless ($opt_S || $opt_G);
 binmode STDOUT;
 my $mark = $opt_G ? 1000000 : 1;
 
-my $dev_branch = ($opt_S || $opt_G) ? $branch : "$branch-Development-$version";
+my $dev_branch = ($opt_S || $opt_G) ? "$branch-Import" : "$branch-Development-$version";
+$dev_branch = $opt_P . $dev_branch if ($opt_P);
 
 print STDERR "Import $dev_branch\n";
 
@@ -757,5 +758,5 @@ git_import
 	print "done\n";
 	print STDERR "Added reference files to $added_ref commit(s)\n";
 	print STDERR "Removed reference files from $removed_ref commit(s)\n";
-	print STDERR "Done importing $dev_branch\n";
+	print STDERR "Done importing ", join(' ', $branch, @ARGV), "\n";
 }
