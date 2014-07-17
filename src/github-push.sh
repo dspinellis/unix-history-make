f5f44d6bed4bbce6c4272aef5d5304a0c985f49f
Diomidis Spinellis <dds@aueb.gr>
2014-07-17 14:06:01 +0300

    Perform the garbage collection at this stage
    Rather than when the importing completes.  This avoids
    burdening the import debugging process.

--- src/github-push.sh
+++ src/github-push.sh
@@ -4,6 +4,9 @@
 
 cd import
 
+#  Optimize structure
+git checkout FreeBSD-Release
+
 # Push to GitHub
 git remote add origin git@github.com:dspinellis/unix-history-repo.git
 git push -fu --all origin
