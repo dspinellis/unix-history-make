1053d0ddcf0dfdd3cec3afcede0c485ad97f9efa
Diomidis Spinellis <dds@aueb.gr>
2016-03-05 14:04:16 +0200

    FreeBSD patch program in vendor state

---
+++ src/patch/pathnames.h
@@ -0,0 +1,12 @@
+/*-
+ * Placed in the public domain by Todd C. Miller <Todd.Miller@courtesan.com>
+ * on July 29, 2003.
+ *
+ * $OpenBSD: pathnames.h,v 1.1 2003/07/29 20:10:17 millert Exp $
+ * $FreeBSD: head/usr.bin/patch/pathnames.h 286346 2015-08-05 22:04:54Z delphij $
+ */
+
+
+#include <paths.h>
+
+#define	_PATH_RED		"/bin/red"
