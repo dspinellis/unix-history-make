7076a032bb9ad313da84c29b9b411ae5af7e093c
Diomidis Spinellis <dds@aueb.gr>
2017-01-08 13:34:58 +0200

    Correct timestamp and portability enhancements
    - Correct byte ordering based on host architecture
    - Use portable integer size types
    - Correct timestamp's epoch and HZ

--- src/ar.h
+++ src/ar.h
@@ -1,9 +1,11 @@
+#include <stdint.h>
+
 #define	ARMAG	0177545
 struct	ar_hdr {
 	char	ar_name[14];
-	long	ar_date;
+	uint32_t ar_date;
 	char	ar_uid;
 	char	ar_gid;
-	int	ar_mode;
-	long	ar_size;
-};
+	int16_t	ar_mode;
+	int32_t	ar_size;
+}__attribute__((packed));
--- src/arcv.c
+++ src/arcv.c
@@ -2,20 +2,44 @@
  * Convert old to new archive format
 */
 
+#include <endian.h>
+#include <stdio.h>
 #include <signal.h>
-#include <ar.h>
+#include <stdint.h>
+#include "ar.h"
 
 #define	omag	0177555
+
+/*
+ * See:
+ * http://www.tuhs.org/Archive/PDP-11/Distributions/research/1972_stuff/Readme
+ * The epoch was determined so that the files in last1120c/rt/libc.sa would
+ * not appear to be after the archive's creation date.
+ * date -d '1971-01-01 00:00 UTC' +%s
+ */
+#define EPOCH_V2 31536000
 
 struct	ar_hdr nh;
 struct
 {
 	char	oname[8];
-	long	odate;
+	uint32_t odate;
 	char	ouid;
 	char	omode;
-	unsigned siz;
-} oh;
+	uint16_t siz;
+} __attribute__((packed)) oh;
+
+#if BYTE_ORDER == LITTLE_ENDIAN
+#define PDPTOHL(x) (((x & 0xffff) << 16) | ((x & 0xffff0000) >> 16))
+#define HTOPDPL(x) PDPTOHL(x)
+#else
+#define SWITCHWORD(x) (((x & 0xff) << 8) | ((x & 0xff00) >> 8))
+#define PDPTOHL(x) \
+		((SWITCHWORD(x & 0xffff) << 16) \
+		 | SWITCHWORD((x & 0xffff0000) >> 16))
+#define HTOPDPL(x) PDPTOHL(x)
+#endif
+
 
 char	*tmp;
 char	*mktemp();
@@ -23,15 +47,16 @@ int	f;
 int	tf;
 union {
 	char	buf[512];
-	int	magic;
+	uint16_t	magic;
 } b;
 
 main(argc, argv)
 char *argv[];
 {
 	register i;
+	char template[] = "/tmp/arcXXXXXX";
 
-	tmp = mktemp("/tmp/arcXXXXX");
+	tmp = mktemp(template);
 	for(i=1; i<4; i++)
 		signal(i, SIG_IGN);
 	for(i=1; i<argc; i++)
@@ -59,7 +84,8 @@ char *fil;
 	b.magic = 0;
 	read(f, (char *)&b.magic, sizeof(b.magic));
 	if(b.magic != omag) {
-		printf("arcv: %s not archive format\n", fil);
+		printf("arcv: %s not archive format (0%o != 0%o)\n", fil,
+				b.magic, omag);
 		close(tf);
 		close(f);
 		return;
@@ -72,11 +98,13 @@ loop:
 		goto out;
 	for(i=0; i<8; i++)
 		nh.ar_name[i] = oh.oname[i];
-	nh.ar_size = oh.siz;
+	/* Convert from 16 bit to 32 bit */
+	nh.ar_size = HTOPDPL(oh.siz);
 	nh.ar_uid = oh.ouid;
 	nh.ar_gid = 1;
 	nh.ar_mode = 0666;
-	nh.ar_date = oh.odate;
+	/* Adjust epoch and HZ */
+	nh.ar_date = HTOPDPL(PDPTOHL(oh.odate) / 60 + EPOCH_V2);
 	n = (oh.siz+1) & ~01;
 	write(tf, (char *)&nh, sizeof(nh));
 	while(n > 0) {
